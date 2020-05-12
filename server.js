cb7529ea3e6ae51aa82c1d9b8b69ab1907d01155
truju <julia.trummer@edu.fh-joanneum.at>
2020-05-12 11:00:08 +0200

    Auto stash before merge of "master" and "origin/master"

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
+//Postgres Database connection
+const { Client } = require('pg');
+
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
-var port = process.env.PORT || 8080
+/*
+Postgres Database Connection
+ */
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
 app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
