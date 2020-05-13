ac7d45fb51729fc3e97475b0edf132993d87019c
truju <julia.trummer@edu.fh-joanneum.at>
2020-05-13 17:03:45 +0200

    Bugfix
    Can not get

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { pool } = require('./config')
+//Postgres Database connection
+const { Client } = require('pg');
 
-const app = express()
+var express = require('express');
+var app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+var port = process.env.PORT || 8080
+app.use(express.static(__dirname));
 
-const getUser = (request, response) => {
-    pool.query('SELECT * FROM User', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-
-app
-    .route('/user')
-    // GET endpoint
-    //.get(getUser())
-    // POST endpoint
-    /*.post(addBook)*/
-
-// Start server
-app.listen(process.env.PORT || 3002, () => {
-    console.log(`Server listening`)
+app.get("/", function (req, res) {
+    res.render("index");
 })
+
+app.listen(port, function () {
+    console.log("app running")
+})
+
+
