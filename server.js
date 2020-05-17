a591f5af8f0c7e0a4db6c231b89d477d663c9bdd
lea <lea.franz@edu.fh-joanneum.at>
2020-05-17 15:37:09 +0200

    changed port

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 
-//var port = process.env.PORT || 8080
-var port = 8080;
+var port = process.env.PORT || 8080
 app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
